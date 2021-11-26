7bb90ff1ce83c1bb8431d5831943b49c22a61aa3
JRaemakers <joostrae@hotmail.nl>
2021-11-26 17:07:04 +0100

    add prettier configuration file

--- client/src/pages/_app.js
+++ client/src/pages/_app.js
@@ -1,9 +1,9 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles/global.css';
-import { Provider as ReduxProvider } from 'react-redux';
-import { store } from '../app/store';
-import { useReduxHydrate } from 'next-redux-hydrate';
-import { Header } from '../components/navigation/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/global.css";
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "../app/store";
+import { useReduxHydrate } from "next-redux-hydrate";
+import { Header } from "../components/navigation/Header";
 
 export const MyApp = ({ Component, pageProps }) => {
   useReduxHydrate({ store });
