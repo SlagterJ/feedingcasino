a84bc6fdf783d46ba11bb56e1031b4948ac80e76
Jordy Slagter <slagterj2001@gmail.com>
2021-12-01 09:34:30 +0100

    Make login work

--- client/src/pages/auth.js
+++ client/src/pages/auth.js
@@ -1,7 +1,39 @@
+import { useRouter } from "next/router";
+import { useFormik } from "formik";
+import axios from "axios";
+import { Input } from "../components/Input";
+
 export const Auth = () => {
+  const router = useRouter();
+
+  const { values, handleChange, handleSubmit } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      axios.post("localhost:8000/api/auth/login", values).then((res) => {
+        // router.push("/");
+        console.log(res.access_token);
+      });
+    },
+  });
+
   return (
     <div>
       <h1 variant="h1">Authentication</h1>
+      <Input
+        title="E-Mail"
+        name="email"
+        value={values.email}
+        onChange={handleChange}
+      />
+      <Input
+        title="Password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      />
     </div>
   );
 };
